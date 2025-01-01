8739a985fd8e7f15de9732a3376fa919f8b2d635
byun0-0 <isly7959@naver.com>
2025-01-01 18:07:27 +0900

    feat: 모바일 환경 OAuth2 개선2

--- src/main/java/com/example/hearurbackend/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/example/hearurbackend/oauth2/CustomSuccessHandler.java
@@ -36,7 +36,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String role = auth.getAuthority();
         String token = jwtUtil.createJwt(username, role, 60 * 60 * 1000 * 10L);
 
-        String redirectUrl = "myapp://login?token=" + URLEncoder.encode(token, StandardCharsets.UTF_8);
+        String redirectUrl = "healthhola://login?token=" + URLEncoder.encode(token, StandardCharsets.UTF_8);
         response.sendRedirect(redirectUrl);
     }
 
