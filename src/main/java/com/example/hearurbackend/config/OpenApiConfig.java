7a7f759fc47e1f69ea892fff77cbcf806837f537
byun0-0 <isly7959@naver.com>
2025-01-21 21:43:23 +0900

    feat: swagger-ui 에서 access token 사용하게 변경

--- src/main/java/com/example/hearurbackend/config/OpenApiConfig.java
+++ src/main/java/com/example/hearurbackend/config/OpenApiConfig.java
@@ -11,19 +11,25 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "JWT";
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("access") // HTTP 헤더 이름 설정
+                .type(SecurityScheme.Type.APIKEY) // API 키 유형으로 변경
+                .in(SecurityScheme.In.HEADER) // 위치는 헤더
+                .description("JWT Token without Bearer");
+
         Info info = new Info()
                 .title("HealthHola 백엔드 API")
                 .version("1.0")
                 .description("구현중");
-        String jwt = "JWT";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        );
+
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemeName, securityScheme);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
+
         return new OpenAPI()
                 .components(components)
                 .addSecurityItem(securityRequirement)
